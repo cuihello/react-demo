10ba364ab0b4197a73526d13a97c014ba60add97
cuihello <1928721464@qq.com>
2019-07-16 19:01:44 +0800

    ok

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,9 @@
+
 const { addLessLoader,override,addDecoratorsLegacy } = require("customize-cra");
 module.exports = override(
     addLessLoader({
         strictMath: true,
         noIeCompat: true,
         localIdentName: "[local]--[hash:base64:5]" // if you use CSS Modules, and custom `localIdentName`, default is '[local]--[hash:base64:5]'.
-    },addDecoratorsLegacy())
+    }),addDecoratorsLegacy()
 );
